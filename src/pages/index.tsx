1298d4c7b642cdeb7fa02331ca4986aa563cc0e9
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-12-06 11:30:30 -0400

    structuring the `home`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,42 @@
-import { styled } from '@/styles'
+import { HomeContainer, Product } from '@/styles/pages/home'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+import camiseta4 from '../assets/camisetas/4.png'
 
 export default function Home() {
   return (
-    <main>
-      <h1>Enick</h1>
-    </main>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta</strong>
+          <span>R$ 29,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta</strong>
+          <span>R$ 29,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta3} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta</strong>
+          <span>R$ 29,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta4} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta</strong>
+          <span>R$ 29,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
