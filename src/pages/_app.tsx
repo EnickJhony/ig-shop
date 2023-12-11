85dc67868cfe4c2e11248f7fa6b3a57e8e8ede4b
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-12-11 14:19:08 -0400

    Solving the font problem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import { globalStyles } from '@/styles/global'
 import { Container, Header } from '@/styles/pages/app'
 import { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
 import LogoImg from '../assets/logo.svg'
+
+const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] })
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
+    <Container className={roboto.className}>
       <Header>
         <Link href="/">
           <Image src={LogoImg} alt="" />
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,7 +1,4 @@
 import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
-
-const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
