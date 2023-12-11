fd36802512ec62571945a432c060a6c17b0539f7
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-12-11 11:12:38 -0400

    Adding Link to Header Image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { globalStyles } from '@/styles/global'
 import { Container, Header } from '@/styles/pages/app'
 import { AppProps } from 'next/app'
 import Image from 'next/image'
+import Link from 'next/link'
 import LogoImg from '../assets/logo.svg'
 
 globalStyles()
@@ -10,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={LogoImg} alt="" />
+        <Link href="/">
+          <Image src={LogoImg} alt="" />
+        </Link>
       </Header>
       <Component {...pageProps} />
     </Container>
