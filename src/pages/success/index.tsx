788fa815eaafcc47508cab4ed85ecd2275088811
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2023-12-13 13:37:17 -0400

    Creating `success` page

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,7 +1,19 @@
+import Link from 'next/link'
+import { ImageContainer, SuccessContainer } from '../../styles/pages/success'
+
 export default function Success() {
   return (
-    <main>
-      <h1>Success</h1>
-    </main>
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Enick Jhony</strong>, sua <strong>Camiseta</strong> já
+        está a caminho da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
   )
 }
